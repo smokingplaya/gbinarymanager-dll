5643f98f44c2afb3ba93193041d54dd6c7a9b2a5
smokingplaya <82664853+smokingplaya@users.noreply.github.com>
2023-05-15 21:50:22 +0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,7 @@
-/*
-	lua gbinarymanager table:
-
-	gbinarymanager.Version (var)
-	gbinarymanager.saveDLL (function)
-*/
+// Просто держу в курсе того что я не программист на расте. Поэтому если увидите говнокодик - не обижайтесь :)
 
 #![feature(c_unwind)]
 
-extern crate steamlocate;
-use steamlocate::SteamDir;
-
-use std::path::PathBuf;
-use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -20,17 +10,22 @@ use gmod::lua::State;
 
 #[macro_use] extern crate gmod;
 
-// main
+fn create_file(name: &str, content: &str) -> std::io::Result<()> {
+	let normal_name : String = format!("{}{}", "garrysmod/lua/bin/", name);
+	let normal_name_str : &str = &normal_name[..];
 
-unsafe fn devprint(lua:State, msg: &str) -> i32 {
-    lua.get_global(lua_string!("print"));
-    lua.push_string(msg);
-    lua.call(1, 1);
-    0
+	let mut file = File::create(normal_name_str)?;
+    file.write_all(content.as_bytes())?;
+
+	Ok(())
 }
 
+unsafe extern "C-unwind" fn save_binary(lua: State) -> i32 {
+	let name = lua.check_string(1);
+	let msg = lua.check_string(2);
 
-unsafe fn safefile(lua: State) -> i32 {
+	let _ = create_file(name.as_ref(), msg.as_ref());
+
 	0
 }
 
@@ -40,13 +35,17 @@ unsafe fn gmod13_open(lua: State) -> i32 {
       override_stdout()
     }
 
-	println!("ЛЮЬИИИТИТИ");
-
     lua.get_global(lua_string!("gbinarymanager"));
 	if lua.is_nil(-1) {
 		lua.pop();
 		lua.new_table();
 	}
+
+	lua.push_string(env!("CARGO_PKG_VERSION"));
+	lua.set_field(-2, lua_string!("version"));
+
+	lua.push_function(save_binary);
+	lua.set_field(-2, lua_string!("SaveBinary"));
 
 	lua.set_global(lua_string!("gbinarymanager"));
 
@@ -54,6 +53,6 @@ unsafe fn gmod13_open(lua: State) -> i32 {
 }
 
 #[gmod13_close]
-fn gmod13_close(lua: gmod::lua::State) -> i32 {
+unsafe fn gmod13_close(_lua: State) -> i32 {
     0
 }
